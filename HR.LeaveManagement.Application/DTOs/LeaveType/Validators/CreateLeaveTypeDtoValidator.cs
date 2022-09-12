9dfb506fa2bf3646040b9d94597d194b01a15115
susanta.rout <susanta.rout@indegene.com>
2022-09-12 23:40:38 +0530

    One Validator Done

---
+++ HR.LeaveManagement.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveType.Validators
+{
+    public class CreateLeaveTypeDtoValidator:AbstractValidator<CreateLeaveTypeDto>
+    {
+        public CreateLeaveTypeDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("{PropertyName} is Required")
+                .NotNull().MaximumLength(50).
+                WithMessage("{PropertyName} must not be exceed 50 charecter");
+
+            RuleFor(x => x.DefaultDays).NotEmpty().WithMessage("{PropertyName} is Required")
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1")
+                .LessThan(100).WithMessage("{PropertyName} must be less 100");
+        }
+    }
+}
